8da455b3e0cf64401391419ee2241a8aa7481168
friendly-machine-4891 <liamhanafee@gmail.com>
2020-08-12 16:10:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,6 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    number = 1
     katz_deli.each do |customer|
       place_on_line.push "#{number}. #{customer}"
       number += 1
